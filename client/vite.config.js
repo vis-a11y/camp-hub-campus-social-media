60a154f1097c39a2fe2448c589fe0b7611c29316
vis-a11y <prajapativishal15537@gmail.com>
2026-04-16 18:48:00 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'http://localhost:5001', // local backend
         changeOrigin: true,
         secure: false,
       },
@@ -17,5 +18,9 @@ export default defineConfig({
         ws: true,
       },
     },
+  },
+
+  build: {
+    chunkSizeWarningLimit: 1000, // 🔥 fixes chunk size warning
   },
 });
